95bcb4334c72c3d93675b70c4ee111383bd90781
Tushar Arora <tushar.arora@greyb.com>
2022-05-27 12:53:45 +0530

    added php

--- add.php
+++ add.php
@@ -1,15 +1,71 @@
+<?php
+if (isset($_POST['submit'])) {
+  if (empty($_POST['title'])) {
+    $title_err = "Title is required";
+  }
+
+  if (empty($_POST['description'])) {
+    $desc_err = "Description is required";
+  }
+  // echo $_POST['title'];
+  // echo "<br>" . $_POST['description'];
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <?php
+  include 'bootstrap.php'
+  ?>
   <title>Add Blog</title>
+  <style>
+    .submitInput {
+      width: 100px;
+      height: 40px;
+      border: none;
+      background: #00b07b;
+      color: white;
+      font-weight: 600;
+      font-size: 18px;
+      border-radius: 10px;
+    }
+  </style>
 </head>
 
 <body>
+  <?php
+  include 'navbar.php'
+  ?>
 
+  <div class="container">
+    <h1>Add your blog here!</h1>
+    <div class="form-container mt-5">
+      <form method="POST" action="">
+        <div class=" mb-3">
+          <label for="title" class="form-label">Add Title</label>
+          <input type="text" class="form-control" id="title" name="title" placeholder="Enter Your Title">
+          <?php if (isset($title_err)) {
+          ?>
+            <div class="alert alert-danger" role="alert">
+              <?php echo $title_err; ?>
+            </div>
+          <?php } ?>
+        </div>
+        <div class="mb-3">
+          <label for="description" class="form-label">Add description</label>
+          <textarea class="form-control" id="description" name="description" rows="5"></textarea>
+          <?php if (isset($desc_err)) {
+          ?>
+            <div class="alert alert-danger" role="alert">
+              <?php echo $desc_err; ?>
+            </div>
+          <?php } ?>
+        </div>
+        <input type="submit" value="Submit" name="submit" class="submitInput float-end">
+      </form>
+    </div>
+  </div>
 </body>
 
 </html>
